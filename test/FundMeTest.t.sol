7d0cc9ad615bc71db9a9d301fa8a2bd99188f97f
rambo <jpramboguanabara@hotmail.com>
2024-11-18 12:58:02 -0300

    feat: first commit

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier
+pragma solidity ^0.8.28;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function testMinimumDollarIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMessageSender() public {
+        console.log(address(this));
+        assertEq(fundMe.i_owner(), address(this));
+    }
+}
