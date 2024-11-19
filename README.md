05b32eaf95b777d5fe1eee25599744c127428e24
rambo <jpramboguanabara@hotmail.com>
2024-11-19 06:48:16 -0300

    feat: add tests + create HelperConfig.s.sol for multi chain deployment

--- README.md
+++ README.md
@@ -1,67 +1,15 @@
-## Foundry
+## Foundry Fund Me
 
-**Foundry is a blazing fast, portable and modular toolkit for Ethereum application development written in Rust.**
+### Tech Stacks
+- Foundry
+- Solidity
+- Chainlink Data Feeds
 
-Foundry consists of:
-
--   **Forge**: Ethereum testing framework (like Truffle, Hardhat and DappTools).
--   **Cast**: Swiss army knife for interacting with EVM smart contracts, sending transactions and getting chain data.
--   **Anvil**: Local Ethereum node, akin to Ganache, Hardhat Network.
--   **Chisel**: Fast, utilitarian, and verbose solidity REPL.
-
-## Documentation
-
-https://book.getfoundry.sh/
-
-## Usage
-
-### Build
-
-```shell
-$ forge build
-```
-
-### Test
-
-```shell
-$ forge test
-```
-
-### Format
-
-```shell
-$ forge fmt
-```
-
-### Gas Snapshots
-
-```shell
-$ forge snapshot
-```
-
-### Anvil
-
-```shell
-$ anvil
-```
-
-### Deploy
-
-```shell
-$ forge script script/Counter.s.sol:CounterScript --rpc-url <your_rpc_url> --private-key <your_private_key>
-```
-
-### Cast
-
-```shell
-$ cast <subcommand>
-```
-
-### Help
-
-```shell
-$ forge --help
-$ anvil --help
-$ cast --help
-```
-# foundry-fund-me
+### Features
+- User is able to send funds to the contract. 
+- User is able to get a list of funders. 
+- Minimum funding amount is 5$. 
+- Only owner can withdraw funds sent to the contract. 
+- Get Ether price and conversion rate integrating with Chainlink Data Feeds. 
+- Contract is deployed using Foundry scripts
+- Contract is tested using Foundry tests
---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,48 @@
+// 1. Deploy mocks when we are on a local anvil chain
+// 2. Keep track of contract address across different chains
+// Sepolia ETH/USD
+// Mainnet ETH/USD
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig {
+    // If we are on a local anvil, we deploy mocks
+    // Otherwise, grab the existing address from the live network
+
+    NetworkConfig public activeNetworkConfig;
+
+    struct NetworkConfig {
+        address priceFeed;
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else if (block.chainid == 1) {
+            activeNetworkConfig = getMainnetEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        // price feed address
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
+    }
+
+    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory mainnetConfig = NetworkConfig({
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        });
+        return mainnetConfig;
+    }
+
+    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
+        // price feed address
+    }
+}
